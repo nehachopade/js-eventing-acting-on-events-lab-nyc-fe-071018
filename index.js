e4170618cc34db71bd8e433b3b37695645dddb20
Neha Chopade <nehachopade22@gmail.com>
2018-08-23 22:48:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function moveDodgerRight(){
   document.addEventListener('keydown', (event) => {
   if (event.which===39) {
     let previousRight = element.style.left
+    console.log(previousRight)
       let previousRightNumber =parseInt(previousRight.replace('px',''),10)
       if (previousRightNumber < 400) {
           let newRightNumber=(previousRightNumber + 1) + "px"
