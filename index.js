dc2ff7192cde86e6baf2349b31a27da9af6e30e6
Neha Chopade <nehachopade22@gmail.com>
2018-08-23 22:33:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ document.addEventListener('keydown', (event) => {
   console.log(event)
   if (event.which===37) {
       let previousLeft = element.style.left
-      if (previousLeft >0) {
-          let previousLeftNumber =parseInt(previousLeft.replace('px',''),10)
+      let previousLeftNumber =parseInt(previousLeft.replace('px',''),10)
+      if (previousLeftNumber >0) {
           let newLeftNumber=(previousLeftNumber -1) + "px"
           element.style.left = newLeftNumber
-  }
-})
+      }
+}})
